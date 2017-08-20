4f3c067ba17dc5fe16f7c8113fd2d73242c2c94f
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-08-20 18:45:23 -0500

    adds border around post, attempted modal

--- client/components/post.jsx
+++ client/components/post.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 var PostEntry = (props) => (
       <div className="Post-entry" >
         <form onSubmit={props.onSubmit}>
-        Post Title: <input id="title" type="text" maxLength="50" name="title"/>
-        Author: <input id="author" type="text" maxLength="50" name="author"/>
-        Image Url: <input id="img" type="url" name="image" />
-        Embed Spotify Id:  <input type="text" name="spotify" value={props.trackId}/>
-        Post: <input type="text" id="post" maxLength="500" size="100" name="post"/>
+        Post Title:<input id="title" type="text" maxLength="50" name="title"/>
+        Author:<input id="author" type="text" maxLength="50" name="author"/>
+        Image-Url:<input id="img" type="url" name="image" />
+        Spotify-Id:<input type="text" name="spotify" value={props.trackId}/>
+        Post:<input type="text" id="post" maxLength="500" size="50" name="post"/>
         <button type="submit">
           Create a new Blog Post
         </button>
